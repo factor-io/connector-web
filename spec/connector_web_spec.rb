14de38642cffaf510cc7446a7843c4be1dc5bcf3
Maciej Skierkowski <maciej@skierkowski.com>
2015-01-22 10:45:18 -0800

    Add basic test coverage

---
+++ spec/connector_web_spec.rb
@@ -0,0 +1,7 @@
+require 'spec_helper'
+
+describe 'web' do
+  it ':: deploy' do
+    expect(true).to be(true)
+  end
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,11 @@
+require "codeclimate-test-reporter"
+require 'rspec'
+require 'factor-connector-api/test'
+
+CodeClimate::TestReporter.start if ENV['CODECLIMATE_REPO_TOKEN']
+
+Dir.glob('./lib/factor/connector/*.rb').each { |f| require f }
+
+RSpec.configure do |c|
+  c.include Factor::Connector::Test
+end
