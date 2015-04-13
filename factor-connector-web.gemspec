503b70da5f3d4970d6bd51935cbab03d998aae07
Maciej Skierkowski <maciej@skierkowski.com>
2015-04-13 15:04:03 -0700

    Adding a listener and tests

--- factor-connector-web.gemspec
+++ factor-connector-web.gemspec
@@ -14,8 +14,10 @@ Gem::Specification.new do |s|
   s.require_paths = ['lib']
 
   s.add_runtime_dependency 'rest-client', '~> 1.8.0'
+  s.add_runtime_dependency 'websockethook', '~> 0.1.01'
 
   s.add_development_dependency 'codeclimate-test-reporter', '~> 0.4.7'
   s.add_development_dependency 'rspec', '~> 3.2.0'
   s.add_development_dependency 'rake', '~> 10.4.2'
+  s.add_development_dependency 'rest-client', '~> 1.8.0'
 end
--- spec/connector_web_spec.rb
+++ spec/connector_web_spec.rb
@@ -1,12 +1,30 @@
 require 'spec_helper'
+require 'rest-client'
 
 describe WebConnectorDefinition do
-  it 'can list for a hook' do
+  it 'can listen for a hook' do
+    @runtime = Factor::Connector::Runtime.new(WebConnectorDefinition)
+    id = "test_#{SecureRandom.hex(4)}"
+    @runtime.start_listener([:hook], id:id)
+    expect(@runtime).to message info:"Registering web hook '#{id}'"
+    expect(@runtime).to message info:'Web hook now open'
+    expect(@runtime).to message info:"Listening on 'http://web.sockethook.io/hook/#{id}'"
+
+    @runtime.stop_listener
   end
 
-  it 'can post' do
+  it 'can trigger for a hook' do
+    @runtime = Factor::Connector::Runtime.new(WebConnectorDefinition)
+    id = "test_#{SecureRandom.hex(4)}"
+    url = "http://web.sockethook.io/hook/#{id}"
+    @runtime.start_listener([:hook], id:id)
+    expect(@runtime).to message info:"Listening on '#{url}'"
+
+    RestClient.post(url,{this_is_a:'test'})
+
+    expect(@runtime).to trigger this_is_a:'test'
+
+    @runtime.stop_listener
   end
 
-  it 'can get' do
-  end
 end
